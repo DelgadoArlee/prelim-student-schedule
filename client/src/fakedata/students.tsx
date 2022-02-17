19d675f0158c632d89e62079040746c5af1c98fb
DelgadoArlee <delgadoarlee@gmail.com>
2022-02-18 00:44:09 +0800

    temporary demo data

---
+++ client/src/fakedata/students.tsx
@@ -0,0 +1,128 @@
+
+export interface Subject{
+    title: string
+    startTime: string
+    endTime: string
+    allDay?: boolean
+    daysOfWeek: number[]
+}
+
+export interface Student{
+    firstName:string
+    lastName: string
+    course:string
+    year: number
+    schedule:Subject[]
+}
+
+export const students: Student[] = [
+    {
+        firstName: "Arlee", 
+        lastName: "Delgado", 
+        course: "BSSE", 
+        year: 3, 
+        schedule:[
+            {
+                title: "Emath2200",
+                startTime: '10:00:00', 
+                endTime:"12:00:00", 
+                daysOfWeek: [5]
+            },
+            {
+                title: "SE2221 - Lec",
+                startTime: '10:00:00', 
+                endTime:"11:00:00", 
+                daysOfWeek: [1, 2]
+            },
+            {
+                title: "SE2221 - Lab",
+                startTime: '10:00:00', 
+                endTime:"13:00:00", 
+                daysOfWeek: [4]
+            },
+            {
+                title: "SE2222 - Lec",
+                startTime: '7:00:00', 
+                endTime:"9:00:00", 
+                daysOfWeek: [4]
+            },
+            {
+                title: "SE2222 - Lab",
+                startTime: '7:00:00', 
+                endTime:"10:00:00", 
+                daysOfWeek: [5]
+            },
+            {
+                title: "SE2223 - Lec",
+                startTime: '7:00:00', 
+                endTime:"10:00:00", 
+                daysOfWeek: [1]
+            },
+            {
+                title: "SE2223 - Lab",
+                startTime: '7:00:00', 
+                endTime:"10:00:00", 
+                daysOfWeek: [2]
+            },
+            {
+                title: "SE2224 - Lec",
+                startTime: '19:00:00', 
+                endTime:"20:30:00", 
+                daysOfWeek: [1, 4]
+            },
+            {
+                title: "SE2224 - Lab",
+                startTime: '13:00:00', 
+                endTime:"16:00:00", 
+                daysOfWeek: [6]
+            },
+            {
+                title: "SE2225 - Lec",
+                startTime: '16:00:00', 
+                endTime:"17:30:00", 
+                daysOfWeek: [1, 5]
+            },
+            {
+                title: "SE2225 - Lab",
+                startTime: '16:00:00', 
+                endTime:"17:30:00", 
+                daysOfWeek: [2, 4]
+            },
+            {
+                title: "SE2226 - Lec",
+                startTime: '14:00:00', 
+                endTime:"16:00:00", 
+                daysOfWeek: [5]
+            },
+            {
+                title: "SE2226 - Lec",
+                startTime: '13:00:00', 
+                endTime:"16:00:00", 
+                daysOfWeek: [2]
+            }
+        
+        ]
+    },
+    {
+        firstName: "jaja", 
+        lastName: "Delgado", 
+        course: "BSSE", 
+        year: 3, 
+        schedule:[
+            {
+                title: "Emath2200",
+                startTime: '10:00:00', 
+                endTime:"12:00:00", 
+                daysOfWeek: [5]
+            },
+            {
+                title: "SE2221 - Lec",
+                startTime: '10:00:00', 
+                endTime:"11:00:00", 
+                daysOfWeek: [1, 2]
+            },
+        
+        
+        ]
+    }
+]
