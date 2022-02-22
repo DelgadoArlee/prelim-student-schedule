8ed34d6b141aeafa52e5fcd361da79e988cff6d4
DelgadoArlee <delgadoarlee@gmail.com>
2022-02-22 08:52:21 +0800

    Refactored redundant code, added border color in comparison view, and added name of student to the colored subjects

--- client/src/components/StudentSchedule/StudentSchedule.tsx
+++ client/src/components/StudentSchedule/StudentSchedule.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import { AppBar, FormControl, Select, SelectChangeEvent, MenuItem, Toolbar, InputLabel, Button } from "@mui/material";
 import { students, Subject, Student } from "../../fakedata/students";
-import Calendar from "./Calendar";
+import Calendar from "../Calendar/Calendar";
 import colors from "../../styles/colors";
+import { border } from '@mui/system';
 
 
-export default function StudentSchedule(){
+export default function StudentSchedule() {
     const [view, setView] = useState<string>("");
     const [studentArray, setStudentArray] = useState<Student[]>([...students]);
     const [schedule, setSchedule] = useState<Subject[]>([]); 
@@ -14,16 +15,28 @@ export default function StudentSchedule(){
 
     const studentOptions = studentArray.map((student, index) => {
         let name = `${student.firstName}  ${student.lastName}`
+        
         return (
             <MenuItem value={index}>{name}</MenuItem>
         )
     })
 
-    const setColor = (subject:Subject, index: number) =>  {
+    const setTitleAndColor = (subjects: Subject[], student: Student, color?: string, borderColor?: string ) =>  {
+
+        return [...subjects].map((subject: Subject, index: number) => {
+            subject.color = (typeof color !== "undefined")? color : colors[index] 
+            subject.title = `${subject.title} - ${student.firstName}  ${student.lastName}`
+
+            if(borderColor){
+                subject.borderColor = borderColor
+            }
+
+            return subject;
+        })
     
-        subject.color = (view === "compare")? "red":colors[index] 
-        return subject;
+        
     };
+
 
 
     const handleOptionChange = (event: SelectChangeEvent) => {
@@ -50,9 +63,9 @@ export default function StudentSchedule(){
         
         const index: number = parseInt(value);
 
-        const subjects: Subject[] = [...studentArray[index].schedule].map(setColor)
-        
-        setSchedule(subjects);
+        const student = studentArray[index];
+
+        setSchedule(setTitleAndColor([...student.schedule], student));
     };
 
     const handleStudentCompareChange = (event: SelectChangeEvent) => {
@@ -70,16 +83,10 @@ export default function StudentSchedule(){
     }
 
     const compare = () => {
-       const scheduleA: Subject[] = [...studentA!.schedule].map( subject => {
-        subject.color = "blue";
-        return subject
-        });
-       const scheduleB: Subject[] = [...studentB!.schedule].map( subject => {
-           subject.color = "red";
-           return subject
-       });
+       const scheduleA: Subject[] = setTitleAndColor([...studentA!.schedule], studentA!, "", "red")
+       const scheduleB: Subject[] = setTitleAndColor([...studentB!.schedule], studentB!, "black", "red" )
 
-        setSchedule([...scheduleA,...scheduleB])
+        setSchedule([...scheduleA, ...scheduleB])
     }
 
     const scheduleView = () => {
