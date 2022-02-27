69e708466b89af04f9b3035e5b4d80260f576ddf
DelgadoArlee <delgadoarlee@gmail.com>
2022-02-27 11:43:08 +0800

    renamed student.ts to post.ts

---
+++ src/api/post.ts
@@ -0,0 +1,27 @@
+import express, { Router, Request, Response, NextFunction } from "express";
+import  { createStudent }  from "../controllers/student";
+import { createSubject } from "../controllers/subject";
+
+const router: Router = express.Router();
+
+router.post('/student', async (req: Request, res: Response, next: NextFunction) => {
+    const  student =  req.body;
+
+    return createStudent( student )
+            .then(data => res.send("Student Added"))
+            .catch((err:any) => res.status(400).send(err));
+});
+
+
+
+router.post('/subject', async (req: Request, res: Response, next: NextFunction) => {
+    const  subject =  req.body;
+
+    return createSubject( subject )
+            .then(data => res.send("Student Added"))
+            .catch((err:any) => res.status(400).send(err));
+});
+
+
+
+export default router;
