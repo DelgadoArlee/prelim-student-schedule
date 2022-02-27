e5b557bed49cb2a73a5ec4c3aa72fda7240cbd69
DelgadoArlee <delgadoarlee@gmail.com>
2022-02-27 11:47:16 +0800

    Created Get request router

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,8 @@ import cors from "cors";
 
 //Routes
 import helloRouter from "./api/hello";
-import studentRouter from "./api/student";
+import postRouter from "./api/post";
+import getRouter from "./api/get";
 
 
 
@@ -20,8 +21,9 @@ app.use(cors());
 app.use(json())
 
 //Routers
-app.use("/api", helloRouter)
-app.use("/student", studentRouter);
+app.use("/api", helloRouter);
+app.use("/post", postRouter);
+app.use("/get", getRouter);
 
 
 export default app;
