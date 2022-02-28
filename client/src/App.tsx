3904db7a01fcc92c6bd27ae7219ffae6043de869
DelgadoArlee <delgadoarlee@gmail.com>
2022-02-28 22:54:13 +0800

    Removed unused code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,29 +2,9 @@ import React, { useEffect, useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import StudentSchedule from './components/StudentSchedule/StudentSchedule';
-import { Student, students } from './fakedata/students';
 
-
-
-const getStudentsFromLS = () => {
-  const data = localStorage.getItem('students');
-  if (data) {
-    return JSON.parse(data);
-  }
-  else {
-    return []
-  }
-}
-
-
-var localStorageHasBeenRun = false;
 
 function App() {
-  if (!localStorageHasBeenRun) {
-    localStorage.setItem('students', JSON.stringify(students))
-    localStorageHasBeenRun = true;
-  }
-
   return (
     <>
       <StudentSchedule />
