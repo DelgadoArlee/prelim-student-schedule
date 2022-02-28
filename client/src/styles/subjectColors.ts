daa655fbead45d4819558348b9fbde182ef2aa33
DelgadoArlee <delgadoarlee@gmail.com>
2022-02-28 22:21:55 +0800

    renamed colors to subjectColors

---
+++ client/src/styles/subjectColors.ts
@@ -0,0 +1,20 @@
+const subjectColors: string[] = [
+    "red", 
+    "orange",
+    "green",
+    "blue", 
+    "purple", 
+    "pink",
+    "brown",
+    "indigo",
+    "skyblue",
+    "maroon",
+    "olive",
+    "darkkhaki",
+    "teal",
+    "black",
+    "orangered",
+    "palegreen"
+]
+
+export default subjectColors ;
