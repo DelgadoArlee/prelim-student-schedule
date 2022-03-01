82632777b70f657ce81964aba011e786041cae01
DelgadoArlee <delgadoarlee@gmail.com>
2022-03-01 13:34:13 +0800

    Added add subject parameter to mapToCards

--- client/src/helper/helpers.ts
+++ client/src/helper/helpers.ts
@@ -2,21 +2,38 @@ import { Subject, SubjectCard } from "../objects/objects";
 import  subjectColors  from "../styles/subjectColors";
 
 //converts Subject to Calendar event object
-const mapToCards = ( subjects: Subject[], color?: string, borderColor?: string ) =>{
+const mapToCards = ( subjects: Subject[], addSubject?: Subject,color?: string, borderColor?: string ) =>{
     
-   return subjects.map( (subject, index) => {
-        const card: SubjectCard = {
-            title: subject.title,
-            startTime: subject.startTime,
-            endTime: subject.endTime,
-            daysOfWeek: subject.days,
-            allDay: subject.allDay,
-            color: (color!)? color : subjectColors[index] ,
-            borderColor: borderColor
-        }
-    
-        return card;
-    })
+    if(addSubject!){
+        return [...subjects, addSubject].map( (subject, index) => {
+            const card: SubjectCard = {
+                title: subject.title,
+                startTime: subject.startTime,
+                endTime: subject.endTime,
+                daysOfWeek: subject.days,
+                allDay: subject.allDay,
+                color: (color!)? color : subjectColors[index] ,
+                borderColor: borderColor
+            }
+        
+            return card;
+        })
+    }else{
+        return subjects.map( (subject, index) => {
+            const card: SubjectCard = {
+                title: subject.title,
+                startTime: subject.startTime,
+                endTime: subject.endTime,
+                daysOfWeek: subject.days,
+                allDay: subject.allDay,
+                color: (color!)? color : subjectColors[index] ,
+                borderColor: borderColor
+            }
+        
+            return card;
+        })
+    }
+   
 } 
 
 export { mapToCards }
